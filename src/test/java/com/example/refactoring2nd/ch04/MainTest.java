2e373371e636549f84c5287fca7e00ef2edbd747
ber01 <ber01@naver.com>
2023-12-24 02:22:39 +0900

    test: zeroDemand, negativeDemand 추가

--- src/test/java/com/example/refactoring2nd/ch04/MainTest.java
+++ src/test/java/com/example/refactoring2nd/ch04/MainTest.java
@@ -1,6 +1,7 @@
 package com.example.refactoring2nd.ch04;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,6 +30,22 @@ class MainTest {
         asia.producers.get(0).setProduction(20);
         assertThat(asia.shortfall()).isEqualTo(-6);
         assertThat(asia.profit()).isEqualTo(292);
+    }
+
+    @Test
+    @DisplayName("수요가 없는 경우")
+    public void zeroDemand() {
+        asia.demand = 0;
+        assertThat(asia.shortfall()).isEqualTo(-25);
+        assertThat(asia.profit()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("수요가 음수인 경우")
+    public void negativeDemand() {
+        asia.demand = -1;
+        assertThat(asia.shortfall()).isEqualTo(-26);
+        assertThat(asia.profit()).isEqualTo(-10);
     }
 
 }
