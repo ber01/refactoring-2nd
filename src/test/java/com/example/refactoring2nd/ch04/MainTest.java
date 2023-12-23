cd3cc1e5d302b57920a5014ad049f85644a2c713
ber01 <ber01@naver.com>
2023-12-24 02:12:05 +0900

    test: BeforeEach 애노테이션 적용

--- src/test/java/com/example/refactoring2nd/ch04/MainTest.java
+++ src/test/java/com/example/refactoring2nd/ch04/MainTest.java
@@ -1,25 +1,27 @@
 package com.example.refactoring2nd.ch04;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MainTest {
 
+    Province asia;
+
+    @BeforeEach
+    public void beforeEach() {
+        asia = new Province(Doc.sampleProvinceData());
+    }
+
     @Test
     public void shortfall() {
-        Province asia = new Province(sampleProvinceData());
         assertThat(asia.shortfall()).isEqualTo(5);
     }
 
     @Test
     public void profit() {
-        Province asia = new Province(sampleProvinceData());
         assertThat(asia.profit()).isEqualTo(230);
-    }
-
-    private static Doc sampleProvinceData() {
-        return Doc.sampleProvinceData();
     }
 
 }
