0fd7ffdfc9c0c2f6580e9cdf2e589ad5f0e512bc
ber01 <ber01@naver.com>
2023-12-24 02:11:16 +0900

    test: shortfall, profit

--- src/test/java/com/example/refactoring2nd/ch04/MainTest.java
+++ src/test/java/com/example/refactoring2nd/ch04/MainTest.java
@@ -7,9 +7,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MainTest {
 
     @Test
-    public void test() {
+    public void shortfall() {
         Province asia = new Province(sampleProvinceData());
         assertThat(asia.shortfall()).isEqualTo(5);
+    }
+
+    @Test
+    public void profit() {
+        Province asia = new Province(sampleProvinceData());
+        assertThat(asia.profit()).isEqualTo(230);
     }
 
     private static Doc sampleProvinceData() {
